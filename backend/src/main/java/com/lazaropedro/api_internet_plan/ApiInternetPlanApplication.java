8f587b30aaf2699a74e7e4a08a63db12e0fad9ea
Lázaro Pedro <lazaro.p806@gmail.com>
2025-08-18 21:24:51 -0300

    backend: initial commit

---
+++ backend/src/main/java/com/lazaropedro/api_internet_plan/ApiInternetPlanApplication.java
@@ -0,0 +1,13 @@
+package com.lazaropedro.api_internet_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInternetPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInternetPlanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lazaropedro/api_internet_plan/ApiInternetPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazaropedro.api_internet_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInternetPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
